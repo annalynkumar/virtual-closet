742d65a5973fa98b2b4afeda1aec5d13be787154
annalynkumar <annalyn.kumar@chicagobooth.edu>
2025-12-06 18:13:31 +0000

    image upload

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,43 +1,50 @@
 class ItemsController < ApplicationController
+  def index
+    # form at the top of the page needs a fresh Item object
+    @item = Item.new
+
+    matching_items = Item.all
+    @list_of_items = matching_items.order(created_at: :desc)
+
+    render template: "item_templates/index"
+  end
+
+  # keep your other actions (new, create, show, etc.) below…
+
 
   def new
-  @the_item = Item.new
-  render template: "item_templates/new"
-end
-
-  def index
-    matching_items = Item.all
-
-    @list_of_items = matching_items.order({ :created_at => :desc })
-
-    render({ :template => "item_templates/index" })
+    @item = Item.new
+    render template: "item_templates/new"
   end
 
   def show
     the_id = params.fetch("path_id")
-
-    matching_items = Item.where({ :id => the_id })
-
+    matching_items = Item.where(id: the_id)
     @the_item = matching_items.at(0)
 
-    render({ :template => "item_templates/show" })
+    render template: "item_templates/show"
   end
 
   def create
-  the_item = Item.new
-  the_item.name = params.fetch("query_name")
-  the_item.category = params.fetch("query_category")
-  the_item.color = params.fetch("query_color")
-  the_item.image_url = params.fetch("query_image_url")
-  the_item.user_id = current_user.id   # ← FIXED HERE
+    the_item = Item.new
+    the_item.name     = params[:item][:name]
+    the_item.category = params[:item][:category]
+    the_item.color    = params[:item][:color]
+    the_item.user_id  = current_user.id
 
-  if the_item.valid?
-    the_item.save
-    redirect_to("/items", { :notice => "Item created successfully." })
-  else
-    redirect_to("/items", { :alert => the_item.errors.full_messages.to_sentence })
+    # Attach uploaded image (if you're using ActiveStorage)
+    if params[:item][:image_file].present?
+      the_item.image.attach(params[:item][:image_file])
+    end
+
+    if the_item.save
+      redirect_to "/items", notice: "Item created successfully."
+    else
+      @item = the_item
+      render template: "item_templates/new"
+    end
   end
-end
+
 
   def update
     the_id = params.fetch("path_id")
@@ -46,7 +53,6 @@ end
     the_item.name = params.fetch("query_name")
     the_item.category = params.fetch("query_category")
     the_item.color = params.fetch("query_color")
-    the_item.image_url = params.fetch("query_image_url")
     the_item.user_id = params.fetch("query_user_id")
 
     if the_item.valid?
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,6 @@
 #  id         :bigint           not null, primary key
 #  category   :string
 #  color      :string
-#  image_url  :string
 #  name       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -14,4 +13,5 @@
 class Item < ApplicationRecord
   belongs_to :user, required: true, class_name: "User", foreign_key: "user_id"
   has_many   :outfit_items, class_name: "OutfitItem", foreign_key: "item_id", dependent: :destroy
+  has_one_attached :image
 end

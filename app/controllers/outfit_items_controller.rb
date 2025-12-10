53ff7e932cd13066b0cbaed6ad7056744df7e291
annalynkumar <annalyn.kumar@chicagobooth.edu>
2025-12-10 19:11:38 +0000

    outfit created redirect

--- app/controllers/outfit_items_controller.rb
+++ app/controllers/outfit_items_controller.rb
@@ -18,18 +18,19 @@ class OutfitItemsController < ApplicationController
   end
 
   def create
-    new_outfit = Outfit.new
-    new_outfit.name = params[:outfit_name]
-    new_outfit.user_id = current_user.id
+  new_outfit = Outfit.new
+  new_outfit.name = params[:outfit_name]
+  new_outfit.user_id = current_user.id
 
-    if new_outfit.save
-      item_ids_array = params[:item_ids] || []
-      item_ids_array.each do |item_id|
-        OutfitItem.create(outfit_id: new_outfit.id, item_id: item_id)
-      end
-      redirect_to "/outfits/#{new_outfit.id}", notice: "Outfit created!"
-    else
-      redirect_to "/outfit_items/new", alert: "Something went wrong."
+  if new_outfit.save
+    item_ids_array = params[:item_ids] || []
+    item_ids_array.each do |item_id|
+      OutfitItem.create(outfit_id: new_outfit.id, item_id: item_id)
     end
+
+    redirect_to "/outfits", notice: "Outfit created!"
+  else
+    redirect_to "/outfit_items/new", alert: "Something went wrong."
   end
+end
 end
